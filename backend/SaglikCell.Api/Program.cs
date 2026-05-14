1f43a70d7340af60fbc0579838dccaaa53900909
Nursena Ateş <mailadresin@example.com>
2026-05-14 22:13:55 +0300

    feat: Inital Db Connection

---
+++ backend/SaglikCell.Api/Program.cs
@@ -0,0 +1,35 @@
+using SaglikCell.Api.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<DbConnectionFactory>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+
+app.UseSwaggerUI();
+
+app.UseCors("AllowFrontend");
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
